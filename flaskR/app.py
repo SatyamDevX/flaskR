a98c457504d12bcd8630e8d80a512719136397e8
SatyamDevX <satyam.development@gmail.com>
2025-04-06 03:00:20 +0530

    auth blueprint and auth htmls are created

--- flaskR/app.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from flaskR import create_app  # Now it should work
-
-app = create_app()
-
-@app.route('/home')
-def helloo():
-   return "create app using application factory function then importing it into app.py file ."
